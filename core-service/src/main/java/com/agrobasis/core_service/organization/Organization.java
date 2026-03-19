fb63834ccf550d037fb1f346c749e83722f93607
guilherme <guilhermerjara@hotmail.com>
2026-03-19 10:46:18 -0400

    feat(organization): implementa entidade com lombok e interface do repositorio (fase GREEN)

--- core-service/src/main/java/com/agrobasis/core_service/organization/Organization.java
+++ core-service/src/main/java/com/agrobasis/core_service/organization/Organization.java
@@ -1,12 +1,17 @@
 package com.agrobasis.core_service.organization;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
+@Getter
+@Setter
+@Table(name = "organization")
 public class Organization {
 
     @Id
--- core-service/src/main/java/com/agrobasis/core_service/organization/OrganizationRepository.java
+++ core-service/src/main/java/com/agrobasis/core_service/organization/OrganizationRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface OrganizationRepository extends JpaRepository<UUID,id> {
+public interface OrganizationRepository extends JpaRepository<Organization,UUID> {
 }
